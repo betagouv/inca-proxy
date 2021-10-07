3f77c909d34fe306b13a98fa9b9cd84f22c264ab
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-10-07 02:44:00 +0200

    fix(scripts): set correct container name

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,5 +6,5 @@ set -e
 echo "Stopping existing Docker containers..."
 docker-compose down
 
-echo "Starting Docker 'app' container..."
-docker-compose up -d app
+echo "Starting Docker 'reverse-proxy' container..."
+docker-compose up -d reverse-proxy
